9ea762a8a9dfe253696471874bd241cc8aebf973
RISHAV <rishavsaha24042003@gmail.com>
2023-11-24 17:14:11 +0530

    Login is working now!

--- App/app/src/main/java/com/rishav/smvduclubapp/LoginActivity.kt
+++ App/app/src/main/java/com/rishav/smvduclubapp/LoginActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.rishav.smvduclubapp.databinding.ActivityLoginBinding
 
 
@@ -14,6 +15,8 @@ class LoginActivity : AppCompatActivity() {
     private val binding: ActivityLoginBinding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
+
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -23,6 +26,7 @@ class LoginActivity : AppCompatActivity() {
 
         supportActionBar?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
 
+        auth = FirebaseAuth.getInstance()
 
         binding.buttonRegister.setOnClickListener {
             startActivity(Intent(this,RegisterActivity::class.java))
@@ -34,15 +38,16 @@ class LoginActivity : AppCompatActivity() {
             val password = binding.editTextLoginPassword.text.toString()
 
             if (email.isNotEmpty() && password.isNotEmpty()) {
-                MainActivity.auth.signInWithEmailAndPassword(email, password)
+                auth.signInWithEmailAndPassword(email, password)
                     .addOnCompleteListener { task ->
                         if (task.isSuccessful) {
                             // The user has successfully logged in.
+                            Toast.makeText(this,"Login Successfully!", Toast.LENGTH_LONG).show()
                             startActivity(Intent(this, MainActivity::class.java))
                             finish()
                         } else {
                             // The user has failed to log in.
-                            Toast.makeText(this, task.exception?.localizedMessage, Toast.LENGTH_LONG).show()
+                            Toast.makeText(this, "FAILED:${task.exception?.localizedMessage}", Toast.LENGTH_LONG).show()
                         }
                     }
             }
